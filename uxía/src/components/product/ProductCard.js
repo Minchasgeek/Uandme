360f1a169957e49cfc979534a91c0867ea755991
Minchasgeek <pedrocarvajallaso@gmail.com>
2026-03-30 13:31:39 +0200

    agotado

--- uxía/src/components/product/ProductCard.js
+++ uxía/src/components/product/ProductCard.js
@@ -3,10 +3,11 @@ import { el } from "../../utils/dom.js";
 export function ProductCard(p) {
   const img = p.images?.[0] || "";
 
-  return el("article", { class: "pcard" }, [
+  return el("article", { class: `pcard ${p.soldOut ? "pcard--soldout" : ""}` }, [
     el("a", { class: "pcard__link", href: `#/producto/${p.id}` }, [
       el("div", { class: "pcard__media" }, [
-        el("img", { src: img, alt: p.name, loading: "lazy" })
+        el("img", { src: img, alt: p.name, loading: "lazy" }),
+        p.soldOut ? el("span", { class: "badge" }, "AGOTADO") : null
       ]),
       el("p", { class: "pcard__name" }, p.name)
     ])
